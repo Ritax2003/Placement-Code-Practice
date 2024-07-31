4d7150cb85a772edd3501dc75e3074ca98b99841
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-07-31 09:39:15 +0530

    Create 3. Pallindrome Linked List.cpp

---
+++ 7. Linked List/3. Pallindrome Linked List.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<ListNode*> lltovec(ListNode* head){
+        vector<ListNode*>vec;
+        while(head!=NULL){
+            vec.push_back(head);
+            head= head->next;
+        }
+        return vec;
+    }
+    bool isPal(vector<ListNode*>v){
+        int i =0,j = v.size()-1;
+        while(i<=j){
+            if(v[i]->val != v[j]->val) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    bool isPalindrome(ListNode* head) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        ListNode* temp = head;
+        vector<ListNode*>v = lltovec(temp);
+        return isPal(v);
+    }
+};
